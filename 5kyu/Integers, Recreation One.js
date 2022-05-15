f3881c8eba5dcefe707e86fb3332fb2d115460ce
JukemJ <chris@dixn.dev>
2022-05-15 17:31:32 -0400

    daily coding challenge

---
+++ 5kyu/Integers, Recreation One.js
@@ -0,0 +1,28 @@
+// 1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+
+// Task
+// Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+
+// We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+
+// Example:
+// list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+// list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+// The form of the examples may change according to the language, see "Sample Tests".
+
+function listSquared(m, n) {
+    let ans = []
+    for(m; m <= n; m++){
+      let divisors = [0]
+      for (let i = 1; i <= Math.sqrt(m); i++){
+        if (m % i == 0){
+          if (i == m/i) divisors.push(i)
+          else divisors.push(i, m / i)
+        }
+      }
+      let sum = divisors.map((x) => x**2).reduce((a,b) => a+b)
+      let root = Math.sqrt(sum)
+      if (Number.isInteger(root)) ans.push([m,sum])
+    }
+    return ans
+}
