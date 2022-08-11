d9ce07b0a0716151249a21ed278db342a37cce44
JukemJ <chris@dixn.dev>
2022-08-11 16:37:43 -0400

    daily coding challenge

---
+++ 7kyu/ToLeetSpeak.js
@@ -0,0 +1,7 @@
+function toLeetSpeak(str) {
+    const alpha ={
+    A : '@', B : '8', C : '(', D : 'D', E : '3', F : 'F', G : '6', H : '#', I : '!', J : 'J', K : 'K', L : '1', M : 'M',
+    N : 'N', O : '0', P : 'P', Q : 'Q', R : 'R', S : '$', T : '7', U : 'U', V : 'V', W : 'W', X : 'X',  Y : 'Y',  Z : '2'
+    }
+    return str.split(' ').map(x=>x.split('').map(y=>alpha[y]).join('')).join(' ')
+  }
